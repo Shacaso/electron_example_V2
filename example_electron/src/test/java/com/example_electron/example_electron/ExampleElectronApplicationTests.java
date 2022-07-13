1855b68b07601c7fd452583e748e7a5fd4b16c79
Araya Alejo <82895574+Shacaso@users.noreply.github.com>
2022-07-13 14:55:48 -0300

    .

---
+++ example_electron/src/test/java/com/example_electron/example_electron/ExampleElectronApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example_electron.example_electron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleElectronApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
